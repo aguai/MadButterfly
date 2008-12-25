42741a670492048ccb637236e9bc8f53457fc0ad
Thinker K.F. Li <thinker@branda.to>
2008-12-25 18:40:27 +0800

    termporary revision

---
+++ examples/dynamic/main.c
@@ -0,0 +1,302 @@
+/*! \file
+ *
+ * svg2code_ex is an example that show programmers how to create a
+ * menu with MadButterfly.
+ *
+ */
+#include <stdio.h>
+#include <mb.h>
+#include <string.h>
+#include "menu.h"
+#include "button.h"
+
+
+typedef struct _engine engine_t;
+struct _engine {
+    X_MB_runtime_t *rt;
+    redraw_man_t *rdman;
+    menu_t *menu;
+    mb_sprite_t *button;
+    int state;
+    co_aix orx,ory;
+    int start_x,start_y;
+    observer_t *obs1,*obs2;
+    shape_t *rect;
+    co_aix rx,ry;
+};
+
+
+typedef struct _mb_button {
+    mb_obj_t obj;
+    engine_t *en;
+    coord_t *root;
+    coord_t *active;
+    coord_t *normal;
+    coord_t *click;
+    void (*press)();
+    void *arg;
+    observer_t *obs_move,*obs_out,*obs_press;
+    mb_progm_t *progm;
+} mb_button_t;
+
+
+#define COORD_SHOW(group) coord_show(group);rdman_coord_changed(en->rdman, group)
+#define COORD_HIDE(group) coord_hide(group);rdman_coord_changed(en->rdman, group)
+
+#define CMOUSE(e) (coord_get_mouse_event(e))
+
+
+static void mb_button_pressed(event_t *evt, void *arg);
+static void mb_button_out(event_t *evt, void *arg);
+
+static void mb_button_move(event_t *evt, void *arg) 
+{
+    mb_button_t *btn = (mb_button_t *) arg;
+    engine_t *en = btn->en;
+
+    
+    printf("Mouse move\n");
+    COORD_SHOW(btn->active);
+#if 0
+    rdman_coord_changed(btn->en->rdman,btn->root);
+#endif
+    rdman_redraw_changed(btn->en->rdman);
+}
+static void mb_button_out(event_t *evt, void *arg) 
+{
+    mb_button_t *btn = (mb_button_t *) arg;
+    engine_t *en = btn->en;
+
+    if (btn->progm) {
+	    mb_progm_abort(btn->progm);
+	    btn->progm = NULL;
+    }
+    printf("mouse out\n");
+    COORD_HIDE(btn->click);
+    COORD_HIDE(btn->active);
+    COORD_SHOW(btn->normal);
+#if 1
+    rdman_coord_changed(btn->en->rdman,btn->normal);
+#endif
+    rdman_redraw_changed(btn->en->rdman);
+}
+
+void mb_button_show_active(event_t *evt, void *arg)
+{
+    mb_button_t *btn = (mb_button_t *) arg;
+    engine_t *en = btn->en;
+
+    COORD_SHOW(btn->active);
+    rdman_coord_changed(btn->en->rdman,btn->root);
+    rdman_redraw_changed(btn->en->rdman);
+}
+
+void mb_button_pressed(event_t *evt, void *arg)
+{
+    mb_button_t *btn = (mb_button_t *) arg;
+    engine_t *en = btn->en;
+    mb_timeval_t start, playing, now;
+    mb_progm_t *progm;
+    mb_word_t *word;
+
+    printf("Pressed\n");
+    if (btn->progm) {
+	    mb_progm_abort(btn->progm);
+	    btn->progm = NULL;
+    }
+    COORD_SHOW(btn->click);
+    COORD_HIDE(btn->active);
+    rdman_coord_changed(en->rdman,btn->root);
+    rdman_redraw_changed(en->rdman);
+
+    btn->progm = progm = mb_progm_new(1, en->rdman);
+    MB_TIMEVAL_SET(&start, 0, 500000);
+    MB_TIMEVAL_SET(&playing, 0, 0);
+    word = mb_progm_next_word(progm, &start, &playing);
+    mb_visibility_new(VIS_HIDDEN, btn->click, word);
+    mb_visibility_new(VIS_VISIBLE, btn->active, word);
+    mb_progm_free_completed(progm);
+    get_now(&now);
+    mb_progm_start(progm, X_MB_tman(en->rt), &now);
+    if (btn->press)
+    	btn->press(btn->arg);
+}
+mb_button_t *mb_button_new(engine_t *en,mb_sprite_t *sp, char *name)
+{
+    mb_button_t *btn = (mb_button_t *) malloc(sizeof(mb_button_t));
+    char *buf = (char *) malloc(strlen(name)+5);
+
+    btn->root = (coord_t *) MB_SPRITE_GET_OBJ(sp, name);
+    printf("btn->root=%x\n",btn->root);
+    sprintf(buf, "%s_normal", name);
+    btn->normal = (coord_t *) MB_SPRITE_GET_OBJ(sp, buf);
+    if (btn->normal == NULL) {
+    	printf("Missing normal button, this is not a correct button\n");
+    }
+    sprintf(buf, "%s_active", name);
+    btn->active = (coord_t *) MB_SPRITE_GET_OBJ(sp, buf);
+    if (btn->active == NULL) {
+    	printf("Missing active button, this is not a correct button\n");
+    }
+    sprintf(buf, "%s_click", name);
+    btn->click = (coord_t *) MB_SPRITE_GET_OBJ(sp, buf);
+    if (btn->click == NULL) {
+    	printf("Missing click button, this is not a correct button\n");
+    }
+    btn->press = NULL;
+    // Show only the normal button
+    COORD_HIDE(btn->active);
+    COORD_HIDE(btn->click);
+    COORD_SHOW(btn->normal);
+    // Move to the same position
+    btn->active->matrix[2] = 200;
+    btn->active->matrix[5] = 200;
+    btn->normal->matrix[2] = 200;
+    btn->normal->matrix[5] = 200;
+    btn->click->matrix[2] = 200;
+    btn->click->matrix[5] = 200;
+    btn->en = en;
+    printf("btn->root=%x\n",CMOUSE(btn->root));
+    btn->obs_move = subject_add_event_observer(CMOUSE(btn->root), EVT_MOUSE_MOVE, mb_button_move,btn);
+    btn->obs_press = subject_add_event_observer(CMOUSE(btn->root), EVT_MOUSE_BUT_PRESS, mb_button_pressed,btn);
+    btn->obs_out = subject_add_event_observer(CMOUSE(btn->root), EVT_MOUSE_OUT, mb_button_out,btn);
+    btn->progm = NULL;
+    rdman_redraw_changed(en->rdman);
+    return btn;
+}
+
+
+void mb_button_add_onClick(mb_button_t *b, void (*h)(void *arg), void *arg)
+{
+    b->press = h;
+    b->arg = arg;
+}
+
+engine_t *engine_init()
+{
+
+    X_MB_runtime_t *rt;
+    rt = X_MB_new(":0.0", 800, 600);
+    engine_t *en = (engine_t *) malloc(sizeof(engine_t));
+
+    en->rt = rt;
+    en->rdman =  X_MB_rdman(rt);
+    return en;
+}
+
+void engine_mainloop(engine_t *en)
+{
+    /*
+     * Start handle connections, includes one to X server.
+     * User start to interact with the application.
+     */
+    X_MB_handle_connection(en->rt);
+
+    /*
+     * Clean
+     */
+    menu_free(en->menu);
+    X_MB_free(en->rt);
+    free(en);
+}
+
+
+static void add_rect_move(event_t *evt, void *arg) 
+{
+    engine_t *en = (engine_t *) arg;
+    mouse_event_t *mev = (mouse_event_t *) evt;
+
+    printf("resize rectangle\n");
+    sh_rect_set(en->rect, en->start_x, en->start_y, mev->x - en->start_x, mev->y-en->start_y,en->rx,en->ry);
+    rdman_shape_changed(en->rdman,en->rect);
+    rdman_redraw_changed(en->rdman);
+}
+
+static void add_rect_release(event_t *evt, void *arg) 
+{
+    engine_t *en = (engine_t *) arg;
+    mouse_event_t *mev = (mouse_event_t *) evt;
+
+    printf("rectangle done\n");
+    subject_remove_observer(CMOUSE(en->rdman->root_coord), en->obs1);
+    subject_remove_observer(CMOUSE(en->rdman->root_coord), en->obs2);
+}
+
+static void add_rect_2(event_t *evt, void *arg) 
+{
+    engine_t *en = (engine_t *) arg;
+    mouse_event_t *mev = (mouse_event_t *) evt;
+    paint_t *color;
+
+    printf("select first point\n");
+    // Add an rect path 
+
+    en->start_x = mev->x;
+    en->start_y = mev->y;
+    subject_remove_observer(CMOUSE(en->rdman->root_coord), en->obs1);
+    subject_remove_observer(CMOUSE(en->rdman->root_coord), en->obs2);
+    en->obs1 = subject_add_event_observer(CMOUSE(en->rdman->root_coord), EVT_MOUSE_MOVE, add_rect_move, en);
+    en->obs2 = subject_add_event_observer(CMOUSE(en->rdman->root_coord), EVT_MOUSE_BUT_RELEASE, add_rect_release, en);
+}
+
+static void add_rect_2_move(event_t *evt, void *arg) 
+{
+    engine_t *en = (engine_t *) arg;
+    mouse_event_t *mev = (mouse_event_t *) evt;
+
+    sh_rect_set(en->rect, mev->x, mev->y, 50,50,en->rx,en->ry);
+    rdman_shape_changed(en->rdman,en->rect);
+    rdman_redraw_changed(en->rdman);
+}
+
+static void add_rect(event_t *evt, void *arg) 
+{
+    engine_t *en = (engine_t *) arg;
+    mouse_event_t *mev = (mouse_event_t *) evt;
+    paint_t *color;
+
+    printf("menut selected\n");
+    en->obs1 = subject_add_event_observer(CMOUSE(en->rdman->root_coord), EVT_MOUSE_BUT_PRESS, add_rect_2, en);
+    en->obs2 = subject_add_event_observer(CMOUSE(en->rdman->root_coord), EVT_MOUSE_MOVE, add_rect_2_move, en);
+    en->rect = rdman_shape_rect_new(en->rdman, mev->x, mev->y, 50 , 50, en->rx, en->ry);
+    // Paint it with color
+    color = rdman_paint_color_new(en->rdman, 0.800000, 0.800000, 0.400000, 1.000000);
+    rdman_paint_fill(en->rdman, color, en->rect);
+    // Add to the stage
+    rdman_add_shape(en->rdman, en->rect, en->menu->root_coord);
+}
+
+
+void test(void *a)
+{
+    printf("Button is pressed.....\n");
+}
+
+
+int main(int argc, char * const argv[]) {
+    subject_t *subject;
+    engine_t *en;
+    mb_button_t *b;
+
+    en = engine_init();
+    en->menu = menu_new(en->rdman, en->rdman->root_coord);
+    en->button = sprite_load("button",en->rdman, en->rdman->root_coord);
+    b = mb_button_new(en, (mb_sprite_t *) en->button, "btn");
+    mb_button_add_onClick(b, test,NULL);
+
+    en->rx = 0;
+    en->ry = 0;
+
+    /*
+     * Register observers to subjects of events for objects.
+     */
+    subject = coord_get_mouse_event(en->menu->rect);
+    subject_add_event_observer(subject,  EVT_MOUSE_BUT_RELEASE, add_rect, en);
+
+
+    engine_mainloop(en);
+
+    return 0;
+}
+
+/* vim: set ts=4 */
---
+++ src/event.c
@@ -0,0 +1,532 @@
+/*! \file
+ * \brief Convenience functions for event relative work.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef UNITTEST
+#include <cairo.h>
+#include "mb_types.h"
+#include "mb_redraw_man.h"
+#include "mb_shapes.h"
+#endif
+
+#define OK 0
+#define ERR -1
+#define FALSE 0
+#define TRUE 1
+
+#define ARRAY_EXT_SZ 64
+
+#define ASSERT(x)
+
+#ifdef UNITTEST
+/* ============================================================ */
+
+typedef struct shape shape_t;
+
+typedef struct cairo cairo_t;
+struct cairo {
+    shape_t *drawed;
+};
+#define cairo_in_fill(cr, x, y) 0
+#define cairo_in_stroke(cr, x, y) 0
+#define cairo_new_path(cr)
+#define cairo_get_target(cr) NULL
+#define cairo_create(target) NULL
+#define cairo_destroy(cr)
+#define cairo_clip(cr)
+#define cairo_fill(cr)
+#define cairo_image_surface_get_data(cr) NULL
+#define cairo_image_surface_get_stride(cr) 1
+
+struct cairo_surface {
+};
+typedef struct cairo_surface cairo_surface_t;
+#define cairo_image_surface_get_width(surface) 0
+#define cairo_image_surface_get_height(surface) 0
+#define cairo_image_surface_create(surface, w, h) NULL
+#define cairo_surface_destroy(surface)
+
+
+typedef float co_aix;
+
+typedef struct _area area_t;
+struct _area {
+    co_aix x, y;
+    co_aix w, h;
+};
+
+struct mb_obj {
+    int obj_type;
+};
+typedef struct mb_obj mb_obj_t;
+
+#define GEF_OV_DRAW 0x1
+#define GEF_HIDDEN 0x2
+
+struct shape {
+    mb_obj_t obj;
+    
+    void *fill, *stroke;
+    struct shape *sibling;
+    int flags;
+    
+    int num_points;
+    co_aix points[32][2];
+};
+enum { MBO_DUMMY,
+       MBO_COORD,
+       MBO_SHAPES=0x1000,
+       MBO_PATH,
+       MBO_TEXT,
+       MBO_RECT
+};
+#define MBO_TYPE(x) (((mb_obj_t *)(x))->obj_type)
+#define IS_MBO_SHAPES(x) (((mb_obj_t *)(x))->obj_type & MBO_SHAPES)
+#define sh_get_geo(x) ((x)->geo)
+static int sh_pos_is_in(shape_t *shape, co_aix x, co_aix y) {
+    int i;
+
+    for(i = 0; i < shape->num_points; i++)
+	if(shape->points[i][0] == x && shape->points[i][1] == y)
+	    return TRUE;
+    return FALSE;
+}
+#define sh_get_flags(shape, mask) ((shape)->flags & mask)
+#define sh_set_flags(shape, mask) do { (shape)->flags |= mask; } while(0)
+#define sh_clear_flags(shape, mask) do { (shape)->flags &= ~(mask); } while(0)
+
+typedef struct coord coord_t;
+struct coord {
+    mb_obj_t obj;
+    
+    coord_t *children;
+    coord_t *sibling;
+    shape_t *shapes;
+};
+
+
+static
+coord_t *postorder_coord_subtree(coord_t *root, coord_t *last) {
+    if(last == NULL)
+	return root;
+}
+
+#define sh_path_draw(path, cr)
+#define sh_text_draw(path, cr)
+#define sh_rect_draw(path, cr)
+
+
+struct redraw_man {
+    cairo_t *cr;
+    int shape_gl_sz;
+    shape_t *shape_gl[32];
+};
+typedef struct redraw_man redraw_man_t;
+#define rdman_get_cr(rdman) ((rdman)->cr)
+#define rdman_get_gen_geos(rdman) (&(rdman)->gen_geos)
+#define rdman_force_clean(rdman) OK
+#define rdman_geos(rdman, geo) NULL
+#define rdman_clear_shape_gl(rdman)		\
+    do {(rdman)->shape_gl_sz = 0; } while(0)
+static int rdman_add_shape_gl(redraw_man_t *rdman, shape_t *shape) {
+    (rdman)->shape_gl[(rdman)->shape_gl_sz++] = shape;
+    return OK;
+}
+#define rdman_get_shape_gl(rdman, idx)		\
+    (rdman)->shape_gl[idx]
+#define rdman_shape_gl_len(rdman) (rdman)->shape_gl_sz
+static shape_t *rdman_shapes(redraw_man_t *rdman, shape_t *last_shape);
+
+
+/* ============================================================ */
+#endif /* UNITTEST */
+
+
+static int _collect_shapes_at_point(redraw_man_t *rdman,
+				   co_aix x, co_aix y) {
+    shape_t *shape;
+    int r;
+    
+    r = rdman_force_clean(rdman);
+    if(r != OK)
+	return ERR;
+
+    rdman_clear_shape_gl(rdman);
+
+    for(shape = rdman_shapes(rdman, (shape_t *)NULL);
+	shape != NULL;
+	shape = rdman_shapes(rdman, shape)) {
+	if(sh_pos_is_in(shape, x, y)) {
+	    r = rdman_add_shape_gl(rdman, shape);
+	    if(r != 0)
+		return ERR;
+	}
+    }
+
+    return OK;
+}
+
+static void draw_shape_path(shape_t *shape, cairo_t *cr) {
+    switch(MBO_TYPE(shape)) {
+    case MBO_PATH:
+	sh_path_draw(shape, cr);
+	break;
+    case MBO_TEXT:
+	sh_text_draw(shape, cr);
+	break;
+    case MBO_RECT:
+	sh_rect_draw(shape, cr);
+	break;
+    }
+}
+
+static int _shape_pos_is_in_cairo(shape_t *shape, co_aix x, co_aix y,
+				  int *in_stroke, cairo_t *cr) {
+    draw_shape_path(shape, cr);
+    if(shape->fill) {
+	if(cairo_in_fill(cr, x, y)) {
+	    *in_stroke = 0;
+	    return TRUE;
+	}
+    }
+    if(shape->stroke) {
+	if(cairo_in_stroke(cr, x, y)) {
+	    *in_stroke = 1;
+	    return TRUE;
+	}
+    }
+    return FALSE;
+}
+
+static
+int _shape_pos_is_in(shape_t *shape, co_aix x, co_aix y,
+		     int *in_stroke, cairo_t *cr) {
+    int r;
+
+    r = sh_pos_is_in(shape, x, y);
+    if(!r)
+	return FALSE;
+
+    r = _shape_pos_is_in_cairo(shape, x, y, in_stroke, cr);
+    cairo_new_path(cr);
+    if(!r)
+	return FALSE;
+
+    return TRUE;
+}
+
+static shape_t *_find_shape_in_pos(redraw_man_t *rdman,
+				   co_aix x, co_aix y, int *in_stroke) {
+    shape_t *shape;
+    cairo_t *cr;
+    int i, r;
+
+    cr = rdman_get_cr(rdman);
+    for(i = rdman_shape_gl_len(rdman) - 1; i >= 0; i--) {
+	shape = rdman_get_shape_gl(rdman, i);
+	if(sh_get_flags(shape, GEF_HIDDEN))
+	    continue;
+	r = _shape_pos_is_in_cairo(shape, x, y, in_stroke, cr);
+	if(r)
+	    return shape;
+    }
+
+    return NULL;
+}
+
+shape_t *find_shape_at_pos(redraw_man_t *rdman,
+			   co_aix x, co_aix y, int *in_stroke) {
+    shape_t *shape;
+    int r;
+
+    r = _collect_shapes_at_point(rdman, x, y);
+    if(r != OK)
+	return NULL;
+
+    shape = _find_shape_in_pos(rdman, x, y, in_stroke);
+    return shape;
+}
+
+/*! \brief Test if an object and descendants cover the position
+ *	specified by x,y.
+ *
+ * \param in_stroke is x, y is on a stroke.
+ */
+int mb_obj_pos_is_in(redraw_man_t *rdman, mb_obj_t *obj,
+		     co_aix x, co_aix y, int *in_stroke) {
+    coord_t *cur_coord, *root;
+    shape_t *shape;
+    int r;
+
+    if(IS_MBO_SHAPES(obj)) {
+	shape = (shape_t *)obj;
+	r = _shape_pos_is_in(shape, x, y, in_stroke, rdman_get_cr(rdman));
+	return r;
+    }
+    root = (coord_t *)obj;
+    for(cur_coord = postorder_coord_subtree(root, NULL);
+	cur_coord != NULL;
+	cur_coord = postorder_coord_subtree(root, cur_coord)) {
+	FOR_COORD_SHAPES(cur_coord, shape) {
+	    r = _shape_pos_is_in(shape, x, y, in_stroke, rdman_get_cr(rdman));
+	    if(r)
+		return TRUE;
+	}
+    }
+    return FALSE;
+}
+
+static
+cairo_t * _prepare_cairo_for_testing(redraw_man_t *rdman) {
+    cairo_surface_t *surface, *rdman_surface;
+    cairo_t *cr;
+    int w, h;
+
+    rdman_surface = cairo_get_target(rdman_get_cr(rdman));
+    w = cairo_image_surface_get_width(rdman_surface);
+    h = cairo_image_surface_get_height(rdman_surface);
+    
+    surface = cairo_image_surface_create(CAIRO_FORMAT_A1, w, h);
+    if(surface == NULL)
+	return NULL;
+
+    cr = cairo_create(surface);
+    if(cr == NULL)
+	cairo_surface_destroy(surface);
+    
+    return cr;
+}
+
+static
+void _release_cairo_for_testing(cairo_t *cr) {
+    cairo_destroy(cr);
+}
+
+static
+void _draw_to_mask(shape_t *shape, cairo_t *cr) {
+    if(sh_get_flags(shape, GEF_OV_DRAW))
+	return;
+    
+    draw_shape_path(shape, cr);
+    cairo_clip(cr);
+    
+    sh_set_flags(shape, GEF_OV_DRAW);
+}
+
+static
+int _fill_and_check(shape_t *shape, cairo_t *cr) {
+    int h, stride;
+    cairo_surface_t *surface;
+    unsigned char *data;
+    int i, sz;
+
+    draw_shape_path(shape, cr);
+    cairo_fill(cr);
+
+    surface = cairo_get_target(cr);
+    data = cairo_image_surface_get_data(surface);
+
+    h = cairo_image_surface_get_height(surface);
+    stride = cairo_image_surface_get_stride(surface);
+
+    sz = stride * h;
+    for(i = 0; i < sz; i++) {
+	if(data[i])
+	    return TRUE;
+    }
+
+    return FALSE;
+}
+
+/*! \brief Is a mb_obj_t overlaid with another mb_obj_t and
+ *	descendants.
+ *
+ * coord is relative less than shapes.  Check areas of coord can
+ * skip sub-trees and avoid useless heavy computation.  For shapes,
+ * it not only check overlay of area.  It also check overlay by
+ * actually drawing on a cairo surface.
+ */
+static
+int _is_obj_objs_overlay(mb_obj_t *obj, mb_obj_t *others_root,
+			 cairo_t *cr) {
+    area_t *area, *candi_area;
+    coord_t *coord, *candi_coord, *root;
+    shape_t *shape, *candi_shape;
+    int obj_is_shape;
+    int r;
+    
+    obj_is_shape = IS_MBO_SHAPES(obj);
+    
+    if(obj_is_shape) {
+	shape = (shape_t *)obj;
+	area = sh_get_area(shape);
+    } else {
+	coord = (coord_t *)obj;
+	area = coord_get_area(coord);
+    }
+	
+    if(IS_MBO_SHAPES(others_root)) {
+	candi_shape = (shape_t *)others_root;
+	candi_area =  sh_get_area(candi_shape);
+	
+	r = areas_are_overlay(area, candi_area);
+	if(!r)
+	    return FALSE;
+	
+	if(!obj_is_shape)
+	    return TRUE;
+	
+	_draw_to_mask(candi_shape, cr);
+	r = _fill_and_check(shape, cr);
+	
+	return r;
+    }
+    
+    ASSERT(IS_MBO_COORD(others_root));
+    
+    root = (coord_t *)others_root;
+    FOR_COORDS_PREORDER(root, candi_coord) {
+	candi_area = coord_get_area(candi_coord);
+	r = areas_are_overlay(area, candi_area);
+	if(!r) {
+	    preorder_coord_skip_subtree(candi_coord);
+	    continue;
+	}
+	
+	FOR_COORD_SHAPES(candi_coord, candi_shape) {
+	    candi_area = sh_get_area(candi_shape);
+	    r = areas_are_overlay(area, candi_area);
+	    if(!r)
+		continue;
+	    
+	    if(!obj_is_shape)
+		return TRUE;
+	    
+	    _draw_to_mask(candi_shape, cr);
+	    r = _fill_and_check(shape, cr);
+	    if(r)
+		return TRUE;
+	}
+    }
+    
+    return FALSE;
+}
+
+static
+void _clear_ov_draw(mb_obj_t *obj) {
+    coord_t *coord, *root;
+    shape_t *shape;
+
+    if(IS_MBO_SHAPES(obj)) {
+	shape = (shape_t *)obj;
+	sh_clear_flags(shape, GEF_OV_DRAW);
+	return;
+    }
+
+    root = (coord_t *)obj;
+    FOR_COORDS_PREORDER(root, coord) {
+	FOR_COORD_SHAPES(coord, shape) {
+	    sh_clear_flags(shape, GEF_OV_DRAW);
+	}
+    }
+}
+
+/*! \brief Test if two objects are overlaid.
+ *
+ * \todo Detect overlay in better way with cairo.
+ * \note This function cost heavy on CPU power.
+ */
+int mb_objs_are_overlay(redraw_man_t *rdman,
+		       mb_obj_t *obj1, mb_obj_t *obj2) {
+    cairo_t *cr;
+    area_t *area;
+    shape_t *shape;
+    coord_t *coord, *root;
+    int r;
+
+    cr = _prepare_cairo_for_testing(rdman);
+
+    if(IS_MBO_SHAPES(obj1)) {
+	shape = (shape_t *)obj1;
+	r = _is_obj_objs_overlay(obj1, obj2, cr);
+	goto out;
+    }
+    
+    root = (coord_t *)obj1;
+    FOR_COORDS_PREORDER(root, coord) {
+	area = coord_get_area(coord);
+	r = _is_obj_objs_overlay((mb_obj_t *)coord, obj2, cr);
+	if(!r) {
+	    preorder_coord_skip_subtree(coord);
+	    continue;
+	}
+
+	FOR_COORD_SHAPES(coord, shape) {
+	    r = _is_obj_objs_overlay((mb_obj_t *)shape, obj2, cr);
+	    if(r)
+		goto out;
+	}
+    }
+    r = FALSE;
+    
+ out:
+    _clear_ov_draw(obj2);	/* marked by _is_obj_objs_overlay() */
+    _release_cairo_for_testing(cr);
+    return r;
+}
+
+#ifdef UNITTEST
+
+#include <CUnit/Basic.h>
+
+static
+redraw_man_t *_fake_rdman(void) {
+    redraw_man_t *rdman;
+    cairo_surface_t *surface;
+
+    rdman = (redraw_man_t *)malloc(sizeof(redraw_man_t));
+    surface = cairo_image_surface_create(CAIRO_FORMAT_A1, 100, 100);
+    rdman->cr = cairo_create(surface);
+    DARRAY_INIT(&rdman->gen_geos);
+    return rdman;
+}
+
+static
+void _free_fake_rdman(redraw_man_t *rdman) {
+    cairo_destroy(rdman->cr);
+    DARRAY_DESTROY(&rdman->gen_geos);
+    free(rdman);
+}
+
+static
+void test_mb_obj_pos_is_in(void) {
+    redraw_man_t *rdman;
+    mb_obj_t *obj;
+
+    rdman = _fake_rdman();
+    CU_ASSERT(rdman != NULL);
+
+    _free_fake_rdman(rdman);
+}
+
+static
+void test_is_obj_objs_overlay(void) {
+}
+
+static
+void test_mb_objs_are_overlay(void) {
+}
+
+CU_pSuite get_event_suite(void) {
+    CU_pSuite suite;
+
+    suite = CU_add_suite("Suite_event", NULL, NULL);
+    CU_ADD_TEST(suite, test_mb_obj_pos_is_in);
+    CU_ADD_TEST(suite, test_is_obj_objs_overlay);
+    CU_ADD_TEST(suite, test_mb_objs_are_overlay);
+
+    return suite;
+}
+
+#endif /* UNITTEST */
