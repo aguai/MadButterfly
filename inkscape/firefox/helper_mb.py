5f3250115905b2f267035f384e20f07e140289cc
wycc <wycc@localhost>
2009-01-29 22:30:46 +0800

    Port all basic scene editor from pyGtk to the firefox

--- inkscape/firefox/helper_mb.py
+++ inkscape/firefox/helper_mb.py
@@ -2,6 +2,7 @@
 from twisted.web import server, resource,soap
 from twisted.internet import reactor,defer
 import os,time
+import traceback
 
 
 
@@ -23,6 +24,43 @@ class Server(soap.SOAPPublisher):
 		print [result]
 		d.callback(result)
 		self.client = None
+	def soap_INSERTKEY(self,layer,n):
+		if self.client == None:
+			os.kill(self.pid,12)
+			time.sleep(1)
+			self.client = Client()
+		try:
+			n = int(n)
+		except:
+			n = 0
+		d = defer.Deferred()
+		self.client.INSERTKEY(layer,n).addCallback(self.generic_return,d).addErrback(self.generic_error,d)
+		return d
+	def soap_EXTENDSCENE(self,layer,n):
+		if self.client == None:
+			os.kill(self.pid,12)
+			time.sleep(1)
+			self.client = Client()
+		try:
+			n = int(n)
+		except:
+			n = 0
+		d = defer.Deferred()
+		self.client.EXTENDSCENE(layer,n).addCallback(self.generic_return,d).addErrback(self.generic_error,d)
+		return d
+	def soap_DELETESCENE(self,layer,n):
+		if self.client == None:
+			os.kill(self.pid,12)
+			time.sleep(1)
+			self.client = Client()
+		try:
+			n = int(n)
+		except:
+			n = 0
+		d = defer.Deferred()
+		self.client.DELETESCENE(layer,n).addCallback(self.generic_return,d).addErrback(self.generic_error,d)
+		return d
+
 	def soap_SCENE(self,n):
 		if self.client == None:
 			os.kill(self.pid,12)
@@ -35,12 +73,28 @@ class Server(soap.SOAPPublisher):
 	def generic_return(self,result,d):
 		print [result]
 		d.callback(result)
+	def generic_error(self,result,d):
+		print [result]
+		d.errback(result)
 	def soap_START(self):
 		if self.client == None:
 			os.kill(self.pid,12)
 			time.sleep(1)
 			self.client = Client()
 		return "OK"
+	def soap_GETDOC(self):
+		try:
+			print "xxxxx"
+			if self.client == None:
+				os.kill(self.pid,12)
+				time.sleep(1)
+				self.client = Client()
+			d = defer.Deferred()
+			self.client.GETDOC().addCallback(self.generic_return,d)
+			print "yyy"
+			return d
+		except:
+			traceback.print_exc()
 		
 
 
@@ -52,9 +106,19 @@ class Client(object):
 		return self.proxy.callRemote('PUBLISH')
 	def SCENE(self,n):
 		return self.proxy.callRemote('SCENE',n)
+	def INSERTKEY(self,layer,n):
+		return self.proxy.callRemote('INSERTKEY',layer,n)
+	def GETDOC(self):
+		return self.proxy.callRemote('GETDOC')
+	def EXTENDSCENE(self,layer,n):
+		return self.proxy.callRemote('EXTENDSCENE',layer,n)
+	def DELETESCENE(self,layer,n):
+		return self.proxy.callRemote('DELETESCENE',layer,n)
+
+os.system("killall -9 inkscape-mb")
 pid = os.fork()	
 if pid==0:
-	os.execvp("inkscape-mb",["inkscape-mb","scene.svg"])
+	os.execvp("inkscape-mb",["inkscape-mb","/tmp/scene.mbsvg"])
 s = Server()
 s.client = None
 s.pid = pid
