39058640d82267aa695834931fc29de589dfd40b
wycc <wycc@localhost>
2009-04-15 08:34:27 +0800

    Implement the MadSwatter integration

--- inkscape/firefox/content/madswatter.js
+++ inkscape/firefox/content/madswatter.js
@@ -7,10 +7,32 @@ function MadSwatter(inkscape)
 
 function MadSwatter_callback(mbsvg,self)
 {
-
-	$(self.inkscape.mbsvg.doc).find("channels").replaceWith(mbsvg.find("channels"));
-	$('#display').tabs('select',0);
+	var ch = $(mbsvg).find("channels");
+	try {
+		var oldch = $(inkscape.mbsvg.doc).find("channels");
+		var newch = $(mbsvg).find("channels");
+		oldch.replaceWith(newch);
+		$('#display').tabs('select',0);
+	} catch(e) {
+		alert(e);
+	}
 	//self.inkscape.updateDocument();
+}
+
+
+
+// Call MadSwatter, we will create file in the /tmp and then create an iframe inside thge madswatter div to
+// load this file. After the MadSwatter finish the job, it should send an event editcb back to us. In this way,
+// we can avoid the security issue.
+function MadSwatter_invoke(dom,callback,self)
+{
+	var f = system_open_write("/tmp/swatter.tmp");
+	var serializer = new XMLSerializer();
+	var cont = serializer.serializeToString(dom[0]);
+
+	f.write(cont,cont.length);
+	f.close();
+	$('#madswatter').html('<iframe width="900" height="700" src="chrome://madswatter/content/editor.xhtml"></iframe>');
 }
 
 MadSwatter.prototype.edit=function()
@@ -18,10 +40,10 @@ MadSwatter.prototype.edit=function()
 	// Generate a new document with the current scene
 	// FIXME: Do we need to do animation filter here?
 	// FIXME: Do we need to support hiearchical animation?
-	$('#display').tabs('select',2);
 	var kk = this.inkscape.mbsvg.generateCurrentSVG();
-	//MadSwatter_invoke(kk,MadSwatter_callback,this);
+	$('#display').tabs('select',2);
+	MadSwatter_invoke(kk,MadSwatter_callback,this);
 	// Simulate the MadSwatter
-	kk.find("metadata").append("<channels></channels>");
-	setTimeout(function () { MadSwatter_callback(kk,this);}, 3000);
+	//kk.find("metadata").append("<channels></channels>");
+	//setTimeout(function () { MadSwatter_callback(kk,this);}, 3000);
 }
