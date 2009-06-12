b425e069380fa073ef9906ee2d5f534ccfce1209
john cylee <john.cylee@gmail.com>
2009-06-12 18:36:15 +0800

    - Fix a minor error in src/sprite.c: should check *(s-1) instead of    *s or the pointer will stop at '/', which is not what we want.
     - Fix various load path errors in examples because libtool put .so
       into .libs directory.
    
     - Fix an error in examples/dynamic/Makefile.am: scene_la_SOURCES
       should be scene.c instead of mytext.c

--- examples/dynamic/hello.c
+++ examples/dynamic/hello.c
@@ -48,7 +48,7 @@ int main(int argc, char * const argv[]) {
     if (argc > 1) 
 	    myApp = MBApp_Init(argv[1]);
     else
-	    myApp = MBApp_Init("scene");
+	    myApp = MBApp_Init(".libs/scene");
     data.currentscene=0;
     MBApp_setData(myApp,&data);
     //b = mb_button_new(myApp, myApp->rootsprite, "btn");
