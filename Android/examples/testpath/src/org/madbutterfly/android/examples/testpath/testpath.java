8c00047d9c8c22a23d4ddefc4ca23bd78a7993f8
Thinker K.F. Li <thinker@branda.to>
2009-12-01 22:55:26 +0800

    Add testpath::onDraw() to add a path on a MBView

--- Android/examples/testpath/src/org/madbutterfly/android/examples/testpath/testpath.java
+++ Android/examples/testpath/src/org/madbutterfly/android/examples/testpath/testpath.java
@@ -3,14 +3,33 @@ package org.madbutterfly.android.examples.testpath;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.graphics.Canvas;
 import org.madbutterfly.MBView;
 import org.madbutterfly.redraw_man;
+import org.madbutterfly.coord;
+import org.madbutterfly.shape;
+import org.madbutterfly.paint;
 
 public class testpath extends Activity
 {
     class myview extends MBView {
 	public myview(Context ctx) {
 	    super(ctx);
+	}
+
+	protected void onDraw(Canvas canvas) {
+	    redraw_man rdman;
+	    shape path;
+	    coord _coord;
+	    paint pnt;
+
+	    rdman = get_rdman();
+	    path = rdman.shape_path_new("M 100 100 L 150 100 L 150 150 z");
+	    _coord = rdman.get_root();
+	    _coord.add_shape(path);
+	    pnt = rdman.paint_color_new(1, 1, 1, 1);
+	    pnt.stroke(path);
+	    rdman.redraw();
 	}
     }
 
