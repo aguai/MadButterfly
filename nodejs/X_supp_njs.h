04d599ff2144cd2844a50d2117a9b32d9661fe4c
Thinker K.F. Li <thinker@branda.to>
2010-06-06 15:27:28 +0800

    Define functions to fill gap of njs runtime.
    We translate functions of njs runtime to instructions of calling
    functions of X runtime.  They are simple macros.

--- nodejs/X_supp_njs.h
+++ nodejs/X_supp_njs.h
@@ -9,4 +9,10 @@ extern void X_njs_MB_free(njs_runtime_t *rt);
 extern njs_runtime_t *X_njs_MB_new(char *display_name, int w, int h);
 extern void *_X_njs_MB_get_X_runtime(njs_runtime_t *rt);
 
+#define X_njs_MB_kbevents(rt) X_MB_kbevents((rt)->xrt)
+#define X_njs_MB_rdman(rt) X_MB_rdman((rt)->xrt)
+#define X_njs_MB_tman(rt) X_MB_tman((rt)->xrt)
+#define X_njs_MB_ob_factory(rt) X_MB_ob_factory((rt)->xrt)
+#define X_njs_MB_img_ldr(rt) X_MB_img_ldr((rt)->xrt)
+
 #endif /* __X_SUPP_NJS_H_ */
