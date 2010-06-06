f34d3573306482f4e96a825ab0b3bdb80eef493f
Thinker K.F. Li <thinker@branda.to>
2010-06-06 13:58:01 +0800

    Upgrade ndoejs plugin to level of a new runtime.
    Original, nodejs plugin is a variant of X runtime.  But, now, I think
    it had better a new runtime that reuse code of X runtime.  It would be
    easier to maintain.

--- nodejs/X_supp_njs.c
+++ nodejs/X_supp_njs.c
@@ -51,7 +51,7 @@ static void
 x_conn_cb(EV_P_ ev_io *iowatcher, int revent) {
     njs_ev_data_t *ev_data = MEM2OBJ(iowatcher, njs_ev_data_t, iowatcher);
     redraw_man_t *rdman;
-    extern void _X_MB_handle_x_event_for_nodejs(X_MB_runtime_t *rt);
+    extern void _X_MB_handle_x_event_for_nodejs(void *rt);
 
     rdman = X_MB_rdman(ev_data->rt);
     _X_MB_handle_x_event_for_nodejs(ev_data->rt);
@@ -67,7 +67,7 @@ timer_cb(EV_P_ ev_timer *tmwatcher, int revent) {
     mb_tman_t *tman;
     redraw_man_t *rdman;
     mb_timeval_t now;
-    extern int _X_MB_flush_x_conn_nodejs(X_MB_runtime_t *rt);
+    extern int _X_MB_flush_x_conn_nodejs(void *rt);
     
     tman = X_MB_tman(ev_data->rt);
     get_now(&now);
@@ -84,43 +84,55 @@ timer_cb(EV_P_ ev_timer *tmwatcher, int revent) {
  *
  * \param rt is a runtime object for X.
  */
-void X_MB_handle_connection_njs(void *rt) {
-    X_MB_runtime_t *_rt = (X_MB_runtime_t *)rt;
-    njs_ev_data_t *ev_data;
+void
+X_njs_MB_handle_connection(njs_ev_data_t *ev_data) {
+    void *rt = ev_data->rt;
     mb_tman_t *tman;
     mb_timeval_t now, tmo;
     ev_tstamp tout;
     int fd;
     int r;
-    extern int _X_MB_get_x_conn_for_nodejs(X_MB_runtime_t *rt);
+    extern int _X_MB_get_x_conn_for_nodejs(void *rt);
 
-    ev_data = (njs_ev_data_t *)malloc(sizeof(njs_ev_data_t));
-    ASSERT(ev_data != NULL);
-    
-    _X_MB_set_data_nodejs(_rt, ev_data);
-    ev_data->rt = _rt;
-    ev_default_loop(0);
-    
     /*
      * Setup watcher for X connection.
      */
-    fd = _X_MB_get_x_conn_for_nodejs(_rt);
+    fd = _X_MB_get_x_conn_for_nodejs(rt);
     ev_io_init(&ev_data->iowatcher, x_conn_cb, fd, EV_READ);
     ev_io_start(&ev_data->iowatcher);
 
     set_next_timeout(ev_data);
 }
 
-void X_MB_stop_njs(void *rt) {
-    X_MB_runtime_t *_rt = (X_MB_runtime_t *)rt;
-    njs_ev_data_t *ev_data;
-
-    ev_data = (njs_ev_data_t *)_X_MB_get_x_conn_for_nodejs(_rt);
-    
+/*! \brief Free njs_ev_data_t.
+ */
+void
+X_njs_MB_free(njs_ev_data_t *ev_data) {
     /*
      * stop IO and timer watcher
      */
     ev_io_stop(&ev_data->iowatcher);
     if(ev_data->enable_timer)
 	ev_timer_stop(&ev_data->tmwatcher);
+}
+
+njs_ev_data_t *
+X_njs_MB_new(char *display_name, int w, int h) {
+    njs_ev_data_t *ev_data;
+    void *rt;
+    
+    ev_data = (njs_ev_data_t *)malloc(sizeof(njs_ev_data_t));
+    ASSERT(ev_data != NULL);
+
+    rt = X_MB_new(display_name, w, h);
+
+    ev_data->rt = rt;
+    ev_data->enable_timer = 0;	/* no timer, now */
+    
+    return ev_data;
+}
+
+void *
+_X_njs_MB_get_runtime(nsj_ev_data_t *ev_data) {
+    return ev_data->rt;
 }
