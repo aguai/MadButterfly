9a8005911b82b84481f9f9466cd3adcd08b10096
Thinker K.F. Li <thinker@branda.to>
2010-06-06 21:51:58 +0800

    New MadButterfly runtime object install of calling function

--- nodejs/testcase.js
+++ nodejs/testcase.js
@@ -3,6 +3,6 @@ var r = mbfly.Hello(" test");
 var sys = require("sys");
 sys.puts(r);
 
-var mb_rt = mbfly.mb_rt(":0.0", 300, 200);
+var mb_rt = new mbfly.mb_rt(":0.0", 300, 200);
 
 setTimeout(function() { sys.puts("timeout"); }, 1000);
