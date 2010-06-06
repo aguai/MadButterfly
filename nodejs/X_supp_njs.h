8f10800f4ddd5fd7c7f0c71452cb1aecede53981
Thinker K.F. Li <thinker@branda.to>
2010-06-06 14:06:42 +0800

    rename ev_data to rt

--- nodejs/X_supp_njs.h
+++ nodejs/X_supp_njs.h
@@ -4,9 +4,9 @@
 struct _njs_runtime;
 typedef struct _njs_runtime njs_runtime_t;
 
-extern void X_njs_MB_handle_connection(njs_runtime_t *ev_data);
-extern void X_njs_MB_free(njs_runtime_t *ev_data);
+extern void X_njs_MB_handle_connection(njs_runtime_t *rt);
+extern void X_njs_MB_free(njs_runtime_t *rt);
 extern njs_runtime_t *X_njs_MB_new(char *display_name, int w, int h);
-extern void *_X_njs_MB_get_runtime(nsj_ev_data_t *ev_data);
+extern void *_X_njs_MB_get_runtime(njs_runtime_t *rt);
 
 #endif /* __X_SUPP_NJS_H_ */
