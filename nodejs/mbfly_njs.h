a488b1494d65152d8f9c31437e71e2c840fce01a
Thinker K.F. Li <thinker@branda.to>
2010-06-07 14:45:01 +0800

    Make root coord availabe for Javascript code

--- nodejs/mbfly_njs.h
+++ nodejs/mbfly_njs.h
@@ -18,6 +18,9 @@ extern "C" {
 #define GET(o, n) (o)->Get(v8::String::New(n))
 
 redraw_man_t *xnjsmb_rt_rdman(v8::Handle<v8::Object> mbrt);
+
+/* From coord.cc */
 v8::Handle<v8::Value> xnjsmb_coord_new(const v8::Arguments &args);
+void xnjsmb_coord_mkroot(v8::Handle<v8::Object> js_rt);
 
 #endif /* __MBFLY_NJS_H_ */
