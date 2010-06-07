47e57fb16dffe12715f453669550766c37bec1b0
Thinker K.F. Li <thinker@branda.to>
2010-06-07 17:24:46 +0800

    Fix issue of im-properly using persistent handler for coord object template.

--- nodejs/testcase.js
+++ nodejs/testcase.js
@@ -4,6 +4,11 @@ var sys = require("sys");
 sys.puts(r);
 
 var mb_rt = new mbfly.mb_rt(":0.0", 300, 200);
-sys.puts(mb_rt.root);
+var root = mb_rt.root;
+sys.puts("root matrix: " +
+	 [root[0], root[1], root[2], root[3], root[4], root[5]]);
+var coord = mb_rt.coord_new(root);
+sys.puts("coord matrix: " + 
+	 [coord[0], coord[1], coord[2], coord[3], coord[4], coord[5]]);
 
 setTimeout(function() { sys.puts("timeout"); }, 1000);
