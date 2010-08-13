c639c84485ab07a56c27ae56b9cb8a3770be226e
wycc <wycc@localhost>
2010-08-14 00:06:00 +0800

    Add mbapp module

--- nodejs/testsvg.js
+++ nodejs/testsvg.js
@@ -1,12 +1,6 @@
 var svg = require("./svg");
-var mbfly = require("mbfly");
-var mb_rt = new mbfly.mb_rt(":0.0", 720,480);
-var ldr = mbfly.img_ldr_new(".");
-var background = mb_rt.rect_new(0, 0, 720, 480, 0, 0);
-var paint = mb_rt.paint_color_new(1, 1, 1, 1);
-paint.fill(background);
-mb_rt.root.add_shape(background);
+var mbapp = require("./mbapp");
 
-svg.loadSVG(mb_rt,mb_rt.root,"test.svg");
-mb_rt.redraw_all();
-mb_rt.flush();
+app = new mbapp.app();
+app.loadSVG("test.svg");
+app.loop();
