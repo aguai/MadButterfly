b4c18aab761f2ab6d94e95cdcf7bde6e4bc9a8e5
Thinker K.F. Li <thinker@branda.to>
2010-08-13 17:51:21 +0800

    Make code clear

--- nodejs/svg.js
+++ nodejs/svg.js
@@ -3,6 +3,10 @@ var sys=require('sys');
 var mbfly = require("mbfly");
 var mb_rt = new mbfly.mb_rt(":0.0", 720,480);
 var ldr = mbfly.img_ldr_new(".");
+var background = mb_rt.rect_new(0, 0, 720, 480, 0, 0);
+var paint = mb_rt.paint_color_new(1, 1, 1, 1);
+paint.fill(background);
+mb_rt.root.add_shape(background);
 
 function MB_loadSVG(mb_rt,root,filename) {
     var doc = libxml.parseXmlFile(filename);
