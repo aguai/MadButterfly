511d55769cedc727b727725caffc8addf0131efa
wycc <wycc@localhost>
2010-08-17 08:31:24 +0800

    Add Y coordiante in the linear animation.

--- nodejs/animate.js
+++ nodejs/animate.js
@@ -4,7 +4,8 @@ function linear_draw() {
     if (this.end == 1) return;
     var percent = (Date.now() - this.starttime)/this.duration;
 	if (percent > 1) percent = 1;
-    this.obj[5] = (this.target-this.startpos)*percent+this.startpos;
+    this.obj[2] = (this.targetx-this.startposx)*percent+this.startposx;
+    this.obj[5] = (this.targety-this.startposy)*percent+this.startposy;
 	this.app.refresh();
 	var self = this;
 	if (percent < 1) {
@@ -14,7 +15,7 @@ function linear_draw() {
 	this.app.refresh();
 	this.obj.animated_linear = null;
 }
-function linear(app,obj,target,duration) {
+function linear(app,obj,targetx,targety,duration) {
     try {
         if (obj.animated_linear) {
 	        obj[5] = obj.animated_linear.target;
@@ -28,8 +29,10 @@ function linear(app,obj,target,duration) {
 	this.obj = obj;
 	this.end = 0;
 	this.starttime = Date.now();
-	this.startpos = obj[5];
-    this.target = target;
+	this.startposx = obj[2];
+	this.startposy = obj[5];
+    this.targetx = targetx;
+	this.targety = targety;
 	this.duration = duration*1000;
 }
 
