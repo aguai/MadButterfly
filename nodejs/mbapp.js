cea1a455c099d813a900af54bc50c23e232c0aed
Thinker K.F. Li <thinker@codemud.net>
2010-08-31 00:06:11 +0800

    mbapp accept arguments for display, width and height

--- nodejs/mbapp.js
+++ nodejs/mbapp.js
@@ -1,16 +1,26 @@
 var mbfly = require("mbfly");
 var svg = require("./svg");
 var sys = require("sys");
-var mb_rt = new mbfly.mb_rt(":0.0", 720,480);
 var ldr = mbfly.img_ldr_new(".");
-var background = mb_rt.rect_new(0, 0, 720, 480, 0, 0);
-var paint = mb_rt.paint_color_new(1, 1, 1, 1);
-paint.fill(background);
-mb_rt.root.add_shape(background);
 
-app=function() {
+
+app=function(display, w, h) {
     var self = this;
-    this.mb_rt = mb_rt;
+    var mb_rt;
+
+    if(typeof display == "undefined")
+	display = ":0.0";
+    if(typeof w == "undefined")
+	w = 720;
+    if(typeof h == "undefined")
+	h = 480;
+    
+    mb_rt = this.mb_rt = new mbfly.mb_rt(display, w, h);
+    var background = mb_rt.rect_new(0, 0, 720, 480, 0, 0);
+    var paint = mb_rt.paint_color_new(1, 1, 1, 1);
+    paint.fill(background);
+    mb_rt.root.add_shape(background);
+
 	this.mb_rt.kbevents.add_event_observer(exports.EVT_KB_PRESS, function(evt) { self.KeyPress(evt);});
 	this.keymap={};
 	this.onKeyPress = null;
