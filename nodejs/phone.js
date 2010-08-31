aad969353d72cd231c76d20e37b012665630dfee
Thinker K.F. Li <thinker@codemud.net>
2010-08-31 09:27:16 +0800

    Change modeline

--- nodejs/phone.js
+++ nodejs/phone.js
@@ -1,4 +1,4 @@
-// -*- indent-tabs-mode: t; tab-width: 4; c-basic-offset: 4; -*-
+// -*- indent-tabs-mode: t; tab-width: 8; c-basic-offset: 4; -*-
 // vim: sw=4:ts=8:sts=4:ai
 var mbapp = require("./mbapp");
 var sys=require("sys");
