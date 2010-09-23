c66aa34b91a7b18305d8eb4b723e10a9e1c7edb3
Thinker K.F. Li <thinker@codemud.net>
2010-09-23 09:55:37 +0800

    Allocate sh_path_t objects from an elmpool

--- include/mb_redraw_man.h
+++ include/mb_redraw_man.h
@@ -50,6 +50,7 @@ struct _redraw_man {
     elmpool_t *geo_pool;
     elmpool_t *coord_pool;
     elmpool_t *shnode_pool;
+    elmpool_t *sh_path_pool;
     elmpool_t *observer_pool;
     elmpool_t *subject_pool;
     elmpool_t *paint_color_pool;
