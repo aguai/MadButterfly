37030ef577ccc99bc13fb00fe9286bd903df122a
Thinker K.F. Li <thinker@codemud.net>
2010-10-27 00:41:25 +0800

    Conditional compile for sh_text in mbaf

--- src/mbaf/mbobject.c
+++ src/mbaf/mbobject.c
@@ -2,6 +2,7 @@
 // vim: sw=4:ts=8:sts=4
 #include "mb_types.h"
 #include "mb_obj.h"
+#include "mb_config.h"
 
 
 void mb_obj_set_pos(mb_obj_t *obj, co_aix x, co_aix y)
@@ -9,8 +10,10 @@ void mb_obj_set_pos(mb_obj_t *obj, co_aix x, co_aix y)
     if (MBO_TYPE(obj) == MBO_COORD) {
 	    coord_x(((coord_t *) obj)) = x;
 	    coord_y(((coord_t *) obj)) = y;
+#ifdef SH_TEXT
     } else if (MBO_TYPE(obj) == MBO_TEXT) {
 	    sh_text_set_pos((shape_t *) obj, x, y);
+#endif
     } else {
 	    return;
     }
@@ -22,8 +25,10 @@ void mb_obj_get_pos(mb_obj_t *obj, co_aix *x, co_aix *y)
     if (MBO_TYPE(obj) == MBO_COORD) {
 	    *x = coord_x((coord_t *) obj);
 	    *y = coord_y((coord_t *) obj);
+#ifdef SH_TEXT
     } else if (MBO_TYPE(obj) == MBO_TEXT) {
 	    sh_text_get_pos((shape_t *) obj, x, y);
+#endif
     } else {
 	    return;
     }
@@ -39,13 +44,17 @@ void mb_obj_set_text(mb_obj_t *obj, const char *text)
 
         FOR_COORD_MEMBERS(g, geo) {
             shape = geo_get_shape(geo);
+#ifdef SH_TEXT
             if(shape->obj.obj_type == MBO_TEXT) {
 		sh_text_set_text(shape, text);
 		return;
             }
+#endif
         }
+#ifdef SH_TEXT
     } else if (MBO_TYPE(obj) == MBO_TEXT) {
 	    sh_text_set_text((shape_t *) obj,text);
+#endif
     } else {
 	    return;
     }
@@ -62,13 +71,17 @@ void mb_obj_get_text(mb_obj_t *obj, char *text,int size)
 
         FOR_COORD_MEMBERS(g, geo) {
             shape = geo_get_shape(geo);
+#ifdef SH_TEXT
             if(shape->obj.obj_type == MBO_TEXT) {
 		sh_text_get_text(shape, text,size);
 		return;
             }
+#endif
         }
+#ifdef SH_TEXT
     } else if (MBO_TYPE(obj) == MBO_TEXT) {
 	    sh_text_get_text((shape_t *) obj,text,size);
+#endif
     } else {
 	    *text = 0;
 	    return;
