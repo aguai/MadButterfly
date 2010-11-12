657e18fdc446fcd750f3d6943feadcda8cfd7d2a
Thinker K.F. Li <thinker@codemud.net>
2010-11-12 16:54:48 +0800

    Migrate testsvg.js to new interface of shift action.

--- nodejs/testsvg.js
+++ nodejs/testsvg.js
@@ -10,36 +10,49 @@ app = new mbapp.app();
 app.loadSVG("test.svg");
 lightbar = app.get("item_lightbar");
 item=1;
-lightbar[5] = app.get("item"+item)[5];
+var target = app.get("item" + item);
+sys.puts(lightbar.center.x);
+sys.puts(lightbar.center.y);
+lightbar.center.move_pnt(target.center);
+// lightbar[5] = app.get("item"+item)[5];
 
 app.files=fs.readdirSync("/tmp/");
 for(i=1;i<10;i++) {
     var o = app.get("item"+i+"text");
-	o.set_text(app.files[i]);
+    o.set_text(app.files[i]);
 }
 
 
 
 
 app.addKeyListener(mbapp.KEY_UP, function() {
-		item = item - 1;
-		if (item == 0) item = 1;
-		else {
-		    var target = app.get("item"+item);
-			var an = new animate.linear(app,lightbar,target[2],target[5],0.3);
-		    an.start();
-		}
+    item = item - 1;
+    if (item == 0)
+	item = 1;
+    else {
+	var target = app.get("item"+item);
+	var shx = target.center.x - lightbar.center.x;
+	var shy = target.center.y - lightbar.center.y;
+	var action = new animate.shift(app, lightbar, shx, shy);
+	var an = new animate.linear(action, 0, 0.3);
+	an.start();
+    }
 });
 
 app.addKeyListener(mbapp.KEY_DOWN, function() {
-	    item = item + 1;
-		if (item == 10) {
-		    item = 9;
-		} else {
-		    var target = app.get("item"+item);
-			var an = new animate.linear(app,lightbar,target[2],target[5],0.3);
-		    an.start();
-		}
+    item = item + 1;
+    if (item == 10) {
+	item = 9;
+    } else {
+	var target = app.get("item"+item);
+	var shx = target.center.x - lightbar.center.x;
+	var shy = target.center.y - lightbar.center.y;
+	var action = new animate.shift(app, lightbar, shx, shy);
+	var an = new animate.linear(action, 0, 0.3);
+	sys.puts(target.center.y);
+	sys.puts(lightbar.center.y);
+	an.start();
+    }
 });
 
 app.loop();
