5f73efe3f34f2fe7a5632ca62a025dfaa03b9fa8
Thinker K.F. Li <thinker@codemud.net>
2010-11-22 13:52:59 +0800

    Fix testcase of animate.c for mb_timer_man_t

--- include/mb_backend_utils.h
+++ include/mb_backend_utils.h
@@ -7,6 +7,6 @@
 /*! \brief A facotry of timer manager implemented with mb_tman_t.
  */
 extern mb_timer_factory_t tman_timer_factory;
-extern mb_tman_t * tman_timer_man_get_tman(mb_timer_man_t *tm_man);
+extern mb_tman_t *tman_timer_man_get_tman(mb_timer_man_t *tm_man);
 
 #endif /* __MB_BACKEND_UTILS_H_ */
