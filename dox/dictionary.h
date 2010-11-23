dc1ff805521f90cf448abc89b173959cdabc4585
Thinker K.F. Li <thinker@codemud.net>
2010-11-23 08:55:22 +0800

    Doc for a list of acronyms.

---
+++ dox/dictionary.h
@@ -0,0 +1,23 @@
+/*! \page dictionary Dictionary
+ *
+ * This page is a list of acronyms and their meaning.
+ *
+ * - \b cb stands for CallBack.
+ * - \b coord stands for COORDinate.
+ * - \b elm stands for ELMent.
+ * - \b geo stands for GEOmetry.
+ * - \b ldr stands for LoaDeR.
+ * - \b man stands for MANager.
+ * - \b mb stands for MadButterfly.
+ * - \b mbe stands for MadButterfly graphic Engine.
+ * - \b ob stands for OBserver.
+ * - \b obj stands for OBJect.
+ * - \b prop stands for PROPerty.
+ * - \b rdman stands for ReDraw MANager.
+ * - \b rect stands for RECTangle.
+ * - \b rm stands for ReMove.
+ * - \b rt stands for RunTime.
+ * - \b sh stands for SHapes.
+ * - \b tman stands for Timer MANager.
+ * - \b val stands for VALue.
+ */
