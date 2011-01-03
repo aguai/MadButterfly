3741db32c4e994192a3961a4af5fc50ecc77cabb
wycc <wycc@localhost>
2011-01-03 08:41:31 +0800

    Update the max frame number to make sure the doRun() will executed to the last frame

---
+++ include/mb_cons_supp.h
@@ -0,0 +1,9 @@
+// -*- indent-tabs-mode: t; tab-width: 8; c-basic-offset: 4; -*-
+// vim: sw=4:ts=8:sts=4
+#ifndef __CONS_SUPP_H_
+#define __CONS_SUPP_H_
+
+typedef void *MB_WINDOW;
+typedef void *MB_DISPLAY;
+
+#endif
